7e5a5cd63e9a0435289ab3214e32266d4746ba2d
Yash <titanmattersbot05@gmail.com>
2024-12-07 22:01:27 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -13,8 +13,24 @@ database = dbclient[DB_NAME]
 
 user_data = database['users']
 
+default_verify = {
+    'is_verified': False,
+    'verified_time': 0,
+    'verify_token': "",
+    'link': ""
+}
 
-
+def new_user(id):
+    return {
+        '_id': id,
+        'verify_status': {
+            'is_verified': False,
+            'verified_time': "",
+            'verify_token': "",
+            'link': ""
+        }
+    }
+    
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
     if found:
@@ -25,6 +41,18 @@ async def present_user(user_id : int):
 async def add_user(user_id: int):
     user_data.insert_one({'_id': user_id})
     return
+
+
+async def db_verify_status(user_id):
+    user = await user_data.find_one({'_id': user_id})
+    if user:
+        return user.get('verify_status', default_verify)
+    return default_verify
+
+async def db_update_verify_status(user_id, verify):
+    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
+
+
 
 async def full_userbase():
     user_docs = user_data.find()
