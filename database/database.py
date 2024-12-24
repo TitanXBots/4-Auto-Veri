882b7ae40a08778c5382b77ccdb03555eaebdd2f
Yash <titanmattersbot05@gmail.com>
2024-12-24 19:09:44 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -7,7 +7,7 @@ import pymongo, os
 from config import DB_URI, DB_NAME
 
 
-dbclient = pymongo.MongoClient(DB_URI)
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 
